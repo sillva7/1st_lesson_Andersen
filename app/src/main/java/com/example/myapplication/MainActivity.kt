fc363ea289cbbceb8e4884efc0dc3bad12dbd2c2
Sergey Vasilev <isi2lva@gmail.com>
2021-11-30 14:38:13 +0500

    Создал новую ветку и кнопку в ней

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val textViewTest:TextView = findViewById(R.id.tVTest)
+        val button:Button = findViewById(R.id.button)// for new branch
+
         textViewTest.text = "1st lesson"
         textViewTest.textSize = 20F // first commit
+
+        button.text = "Knopa"
     }
 }
